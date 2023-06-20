59fb84178373247230fc162637258a5e64b1666d
steven castaño <stevencas0002@gmail.com>
2023-06-19 23:44:53 -0500

    Version 1.1 Conexion a base de datos

--- registro.php
+++ registro.php
@@ -1,11 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 $servername = "localhost";
-$username = "trabajador";
-$password = "19092000";
+$username = "root";
+$password = "";
 $dbname = "appmascotas";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -14,22 +10,21 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
-$nombres = $_POST['nombres'];
+$nombre = $_POST['nombre'];
 $apellidos = $_POST['apellidos'];
 $correo = $_POST['correo'];
-$fecha_nacimiento = $_POST['fecha_nacimiento'];
-$contraseña = $_POST['confirmar_contraseña'];
+$fecha_nacimiento = $_POST['fecha-nacimiento'];
+$contrasena = $_POST['contrasena'];
 
 $sql = "INSERT INTO registro (nombres, apellidos, correo, fecha_nacimiento, contraseña)
         VALUES (?, ?, ?, ?, ?)";
 
 $stmt = $conn->prepare($sql);
-
-$stmt->bind_param("sssss", $nombres, $apellidos, $correo, $fecha_nacimiento, $contraseña);
+$stmt->bind_param("sssss", $nombre, $apellidos, $correo, $fecha_nacimiento, $contrasena);
 
 if ($stmt->execute()) {
     // Redirigir al usuario a la página de inicio de sesión
-    header("Location: C:\wamp64\www\ProyectoINGsoftware\Login.html");
+    header("Location: Login.html");
     exit(); // Asegurar que el script se detenga después de la redirección
 } else {
     echo "Error al registrar: " . $stmt->error;
